df7f392e1647519622b04567f055ac46c6188928
tyra171 <P172171my>
2021-02-26 17:27:48 +0000

    Script Updated

---
+++ lab-05/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+myHeight.addEventListener('input', ev => {
+  document.documentElement.style.setProperty('--height', `${myHeight.value}%`);
+});
+
+myBG.addEventListener('input', ev => {
+  document.documentElement.style.setProperty('--bg-colour', myBG.value);
+});
+myFG.addEventListener('input', ev => {
+  document.documentElement.style.setProperty('--fg-colour', myFG.value);
+});
